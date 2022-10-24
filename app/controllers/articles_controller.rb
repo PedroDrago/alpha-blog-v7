dc000e342c06f13ee54fd6926e232af9ccc81b75
drago-ubuntu <pedrodrago.profissional@gmail.com>
2022-10-24 07:33:43 -0300

    refactor: remove redundant methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,53 +1,59 @@
 class ArticlesController < ApplicationController
-  
-  def show
-    @article = Article.find(params[:id])
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  def show
+    
   end
 
   def index
     @articles = Article.all
-
   end
+
 
   def new
     @article = Article.new
-
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(set_params)
     if @article.save
       flash[:notice] = 'Article created successfully'
       redirect_to @article
     else
       render 'new'
     end
-    
-
   end
 
-  def edit
-    @article = Article.find(params[:id])
 
+  def edit
+    
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    
+    if @article.update(set_params)
       flash[:notice] = 'Article updated successfully'
       redirect_to @article
     else
       render 'edit'
     end
-
   end
 
+  
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = 'Article deleted successfully'
     redirect_to articles_path
+  end
 
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def set_params
+    params.require(:article).permit(:title, :description)
   end
 end
